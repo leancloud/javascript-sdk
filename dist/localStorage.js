eeea8da606635909ffe95421615bcdf2d3075482
dennis zhuang <killme2008@gmail.com>
2015-06-26 12:04:50 +0800

    Updated dist

---
+++ dist/localStorage.js
@@ -0,0 +1,20 @@
+function Storage(){
+	this.length = 0;
+}
+Storage.prototype.getItem = function (key) {
+    return null;
+};
+
+Storage.prototype.setItem = function (key, val) {
+};
+
+Storage.prototype.removeItem = function (key) {
+};
+
+Storage.prototype.clear = function () {
+};
+
+Storage.prototype.key = function (i) {
+};
+
+exports.localStorage = new Storage();
