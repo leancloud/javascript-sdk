b1e10addf5764fbcbbce829553075aa11e1d09b7
dennis zhuang <killme2008@gmail.com>
2015-06-29 17:13:35 +0800

    Updated bigquery test

--- test/bigquery.js
+++ test/bigquery.js
@@ -1,16 +1,29 @@
 describe('bigquery', function() {
   describe('startJob', function(){
-    this.timeout(10000);
-    it('return job id.', function(done) {
+    this.timeout(20000);
+    it('return job id and query job status.', function(done) {
       AV.BigQuery.startJob({
-        sql: "select * from _User",
+        sql: "select * from `_User`",
         saveAs: {
           className: 'BigQueryResult',
           limit:1
         }
       }).then(function(id) {
-        console.log(id);
-        done();
+        expect(id).to.be.ok();
+        setTimeout(function() {
+          try{
+          var q = new AV.BigQuery.JobQuery(id, AV.User);
+          q.find().then(function(results) {
+            console.dir(results);
+            expect(results).to.not.be.empty();
+            done();
+          }, function(err) {
+            throw err;
+          });
+          }catch(e) {
+              console.dir(e);
+          }
+        }, 10000);
       }, function(err) {
         throw err;
       });
