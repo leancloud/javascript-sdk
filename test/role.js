3f67d5fe2792b8376c0e8658a045c9bef83e4dc1
leeyeh <lee55962698@gmail.com>
2015-09-17 19:46:30 +0800

    Add test for AV.Role.

---
+++ test/role.js
@@ -0,0 +1,24 @@
+describe("Role", function() {
+  describe("constructor", function() {
+    it("normal usage", function() {
+      var acl = new AV.ACL();
+      var role = new AV.Role('foo', acl);
+      expect(role.getName()).to.be('foo');
+      expect(role.getACL()).to.be(acl);
+    });
+    it("acl is optional", function() {
+      var role = new AV.Role('foo');
+      expect(role.getName()).to.be('foo');
+      expect(role.getACL().toJSON()).to.eql({
+        '*': {
+          read: true
+        }
+      });
+    });
+    it("type check", function() {
+      expect(function() {
+        new AV.Role('foo', {});
+      }).to.throwError();
+    });
+  });
+});
