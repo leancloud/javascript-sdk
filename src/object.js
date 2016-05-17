657f618a52cdc5d48457501d3062ed1d945f4b7e
jysperm <jysperm@gmail.com>
2016-05-17 18:23:12 +0800

    fix(Object.fetch): fetchOptions.include.join is not a function

--- src/object.js
+++ src/object.js
@@ -804,7 +804,7 @@ module.exports = function(AV) {
         options = arguments[1] || {};
       }
 
-      if (fetchOptions && fetchOptions.include && fetchOptions.include.length > 0) {
+      if (fetchOptions && fetchOptions.include && _.isArray(fetchOptions.include)) {
         fetchOptions.include = fetchOptions.include.join(',');
       }
 
