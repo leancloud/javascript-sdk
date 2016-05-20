76e1ee313ae3f907252ed6fc56a9d8f76980c07c
尼奥 <1988wangxiao@gmail.com>
2016-05-20 17:34:20 +0800

    Change demo code style and add eslint.

--- demo/test-es6.js
+++ demo/test-es6.js
@@ -1,6 +1,16 @@
+/*
+  请参考 README.md 中的开发方式，
+  执行 gulp dev 该文件会被编译为 test-es5.js 并自动运行此文件
+*/
+
+/* eslint no-console: ["error", { allow: ["log"] }] */
+/* eslint no-undef: ["error", { "AV": true }] */
+
 // 初始化
 const appId = 'a5CDnmOX94uSth8foK9mjHfq-gzGzoHsz';
 const appKey = 'Ue3h6la9zH0IxkUJmyhLjk9h';
+const AV = AV || {};
+
 AV.init({ appId, appKey });
 
 // 基本存储
@@ -8,7 +18,7 @@ const TestClass = AV.Object.extend('TestClass');
 const testObj = new TestClass();
 testObj.set({
   name: 'hjiang',
-  phone: '123123123'
+  phone: '123123123',
 });
 
 testObj.save().then(() => {
@@ -20,7 +30,7 @@ testObj.save().then(() => {
 
 // 存储文件
 const base64 = 'd29ya2luZyBhdCBhdm9zY2xvdWQgaXMgZ3JlYXQh';
-var file = new AV.File('myfile.txt', { base64: base64 });
+const file = new AV.File('myfile.txt', { base64 });
 file.metaData('format', 'txt file');
 file.save().then((data) => {
   console.log(data);
@@ -34,4 +44,3 @@ query.equalTo('name', 'hjiang');
 query.find().then((list) => {
   console.log(list);
 });
-
