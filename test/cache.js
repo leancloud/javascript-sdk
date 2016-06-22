b315404c9eb9b12626b1dfa19d47cb55d3b3eb76
尼奥 <1988wangxiao@gmail.com>
2016-06-23 03:36:55 +0800

    Add Cache module unit test

--- test/cache.js
+++ test/cache.js
@@ -5,14 +5,32 @@ var wait = function wait(time) {
   });
 };
 
-describe('Cache', function () {
+describe('Cache sync', function () {
   var getValue = function getValue() {
     return Cache.get('__test');
   };
-  it('get/set', function () {
-    return Cache.set('__test', 1).then(getValue).then(function (value) {
+  it('get/set sync', function () {
+    Cache.set('__test', 1);
+    var value = getValue();
+    expect(value).to.be(1);
+    Cache.set('__test', '1', 100);
+    value = getValue();
+    expect(value).to.be('1');
+    wait(110).then(getValue)
+    .then(function(data) {
+      expect(data).to.be(null);
+    });
+  });
+});
+
+describe('Cache async', function () {
+  var getValue = function getValue() {
+    return Cache.getAsync('__test');
+  };
+  it('get/set async', function () {
+    return Cache.setAsync('__test', 1).then(getValue).then(function (value) {
       expect(value).to.be(1);
-      return Cache.set('__test', '1', 100).then(getValue);
+      return Cache.setAsync('__test', '1', 100).then(getValue);
     }).then(function (value) {
       expect(value).to.be('1');
       return wait(110).then(getValue);
