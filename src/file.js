8ddbff1ff3e412061df5688eee5359e7e92ef351
尼奥 <1988wangxiao@gmail.com>
2016-07-28 17:46:43 +0800

    code review

--- src/file.js
+++ src/file.js
@@ -356,11 +356,8 @@ module.exports = function(AV) {
       try {
         owner = AV.User.current();
       } catch (error) {
-        if (error) {
-          throw new Error(error);
-        } else {
-          console.warn('Get current user failed. It seems this runtime use an async storage system, please new AV.File in the callback of AV.User.currentAsync().');
-        }
+        throw new Error(error);
+        console.warn('If this runtime use an async storage system, please new AV.File in the callback of AV.User.currentAsync().');
       }
     }
 
