4602e115873222061c45a3b6221788c5cc05a6a3
leeyeh <lee55962698@gmail.com>
2017-04-13 11:47:37 +0800

    fix: lowercase default server urls

--- src/init.js
+++ src/init.js
@@ -26,7 +26,7 @@ function getDefaultServerURLs(appId, region) {
       // US
       return fillServerURLs('https://us-api.leancloud.cn');
     default:
-      id = appId.slice(0, 8);
+      id = appId.slice(0, 8).toLowerCase();
       return {
         push: `https://${id}.push.lncld.net`,
         stats: `https://${id}.stats.lncld.net`,
