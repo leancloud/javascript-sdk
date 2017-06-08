8426c9e4cf4946057f72f07392d1c04969c960c1
leeyeh <lee55962698@gmail.com>
2017-06-08 11:44:17 +0800

    feat(File): support Stream in node

--- src/uploader/s3.js
+++ src/uploader/s3.js
@@ -1,5 +1,14 @@
 const request = require('superagent');
-const AVPromise = require('../promise');
+const Promise = require('../promise');
+
+const handleError = (error, res) => {
+  if (res) {
+    error.statusCode = res.status;
+    error.responseText = res.text;
+    error.response = res.body;
+  }
+  return error;
+};
 
 module.exports = function upload(uploadInfo, data, file, saveOptions = {}) {
   file.attributes.url = uploadInfo.url;
@@ -9,20 +18,19 @@ module.exports = function upload(uploadInfo, data, file, saveOptions = {}) {
     // 海外节点，针对 S3 才会返回 upload_url
     const req = request('PUT', uploadInfo.upload_url)
       .set('Content-Type', file.get('mime_type'))
-      .send(data);
     if (saveOptions.onprogress) {
       req.on('progress', saveOptions.onprogress);
     }
-    req.end((err, res) => {
-      if (err) {
-        if (res) {
-          err.statusCode = res.status;
-          err.responseText = res.text;
-          err.response = res.body;
-        }
-        return reject(err);
-      }
-      resolve(file);
+    req.on('response', (res) => {
+      if (res.ok) return resolve(file);
+      reject(handleError(res.error, res));
     });
+    req.on('error', (err, res) => reject(handleError(err, res)));
+    if (require('stream') && data instanceof require('stream')) {
+      // data.pipe(req);
+      throw new TypeError('Saving an AV.File from a Stream to S3 is not yet supported');
+    } else {
+      req.send(data).end();
+    }
   });
 };
