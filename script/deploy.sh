2bf509cb547be95b0da4d2b424593ac077f1a0d9
leeyeh <lee55962698@gmail.com>
2018-05-16 17:55:27 +0800

    chore(build): auto deploy demos

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,11 +3,13 @@ echo "Deploy docs to github pages.";
 npm run docs;
 mkdir gh_pages;
 cp -r docs gh_pages/;
+cp -r dist/av* gh_pages/;
+cp -r demo gh_pages/;
 cd gh_pages && git init;
 git config user.name "leancloud-bot";
 git config user.email "ci@leancloud.cn";
 git add .;
-git commit -m "Deploy docs to Github Pages [skip ci]";
+git commit -m "Deploy docs and demos to Github Pages [skip ci]";
 git push -qf https://${TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git master:gh-pages;
 echo "done.";
 cd ..
