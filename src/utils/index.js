6a20794bc58c49db7d510683b822c6a7f534028b
Lee Yeh <lee55962698@gmail.com>
2019-06-20 16:28:57 +0800

    refactor: simplify parseDate

--- src/utils/index.js
+++ src/utils/index.js
@@ -181,29 +181,7 @@ const inherits = function inherits(parent, protoProps, staticProps) {
   return child;
 };
 
-function parseDate(iso8601) {
-  var regexp = new RegExp(
-    '^([0-9]{1,4})-([0-9]{1,2})-([0-9]{1,2})' +
-      'T' +
-      '([0-9]{1,2}):([0-9]{1,2}):([0-9]{1,2})' +
-      '(.([0-9]+))?' +
-      'Z$'
-  );
-  var match = regexp.exec(iso8601);
-  if (!match) {
-    return null;
-  }
-
-  var year = match[1] || 0;
-  var month = (match[2] || 1) - 1;
-  var day = match[3] || 0;
-  var hour = match[4] || 0;
-  var minute = match[5] || 0;
-  var second = match[6] || 0;
-  var milli = match[8] || 0;
-
-  return new Date(Date.UTC(year, month, day, hour, minute, second, milli));
-}
+const parseDate = iso8601 => new Date(iso8601);
 
 const setValue = (target, key, value) => {
   // '.' is not allowed in Class keys, escaping is not in concern now.
