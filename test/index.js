f1c9bdb250848be8809c89e19078650b69f3cca8
Hong Jiang <j@1byte.io>
2019-09-23 15:56:25 +0800

    test: only test file API when using real backend

--- test/index.js
+++ test/index.js
@@ -1,20 +1,25 @@
 require('should');
 require('./test.js');
 require('./av.js');
-require('./geopoints.js');
+
+require('./acl.js');
+require('./cache');
 require('./cloud.js');
+require('./conversation.js');
+require('./error.js');
+require('./geopoints.js');
 require('./hooks.js');
+require('./leaderboard.js');
 require('./object.js');
 require('./query.js');
-require('./user.js');
-require('./cache');
-require('./file.js');
-require('./error.js');
-// require('./captcha.js');
-require('./acl.js');
 require('./role.js');
-require('./status.js');
-require('./sms.js');
 require('./search.js');
-require('./conversation.js');
-require('./leaderboard.js');
+require('./sms.js');
+require('./status.js');
+require('./user.js');
+
+if (process.env.REAL_BACKEND !== undefined) {
+  require('./file.js');
+}
+
+// require('./captcha.js');
