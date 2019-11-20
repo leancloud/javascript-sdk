e9a37b4c63796101c24f40e124abc66612a32cef
Lee Yeh <lee55962698@gmail.com>
2019-11-20 11:46:05 +0800

    chore(release): remvoe bower versoin checking as bower is not supported any more

--- script/check-version.js
+++ script/check-version.js
@@ -1,4 +1,3 @@
 #!/usr/bin/env node
 const assert = require('assert');
 assert(require('../').version === require('../package.json').version);
-assert(require('../bower.json').version === require('../package.json').version);
