54cb5c23e0e787b1280353a9d69932ab5c86b708
Jang Rush <weakish@gmail.com>
2019-12-09 11:29:51 +0800

    docs(ts): more accurate type for changedAttributes
    related ticket: 22498

--- storage.d.ts
+++ storage.d.ts
@@ -322,7 +322,7 @@ export class Object extends BaseObject {
   bitOr(attributeName: string, item: number): this;
   bitXor(attributeName: string, item: number): this;
   change(options: any): this;
-  changedAttributes(diff: any): boolean;
+  changedAttributes(diff?: { [key: string]: any }): boolean;
   clear(options: any): any;
   revert(keys?: string | string[]): this;
   clone(): this;
