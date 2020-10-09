a230e625b32ed0201ecba9dd43da6b314833de59
sdjdd <i@sdjdd.com>
2020-10-09 15:26:54 +0800

    feat: deploy v5 docs as well

--- script/deploy.sh
+++ script/deploy.sh
@@ -6,6 +6,14 @@ cp -r docs gh_pages/;
 mkdir gh_pages/dist;
 cp -r dist/av* gh_pages/dist/;
 cp -r demo gh_pages/;
+
+echo "Deploy doss(v5) to github pages."
+npm install -g typedoc@0.19
+git checkout v5
+rm -rf docs
+typedoc --options typedoc.json
+cp -r docs gh-pages/docs-v5
+
 cd gh_pages && git init;
 git config user.name "leancloud-bot";
 git config user.email "ci@leancloud.cn";
