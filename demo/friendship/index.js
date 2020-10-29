68d2886bff07868e257d197ac50b10a3af0e9039
sdjdd <i@sdjdd.com>
2020-10-29 10:16:01 +0800

    demo: update friendship demo

--- demo/friendship/index.js
+++ demo/friendship/index.js
@@ -1,18 +1,10 @@
-const events = [];
-const listeners = [];
-
-function broadcast(event, value) {
-  events.push([event, value]);
-  listeners.forEach((listener) => listener(event, value));
-}
-
-Vue.component('friend-request-to', {
-  template: '#friend-request-to',
-  props: ['username', 'status'],
+Vue.component('friend-item', {
+  template: '#friend-item',
+  props: ['username'],
 });
 
-Vue.component('friend-request-from', {
-  template: '#friend-request-from',
+Vue.component('request-item', {
+  template: '#request-item',
   props: ['username', 'status'],
 });
 
@@ -20,107 +12,178 @@ Vue.filter('statusName', (value) => {
   switch (value) {
     case 'pending':
       return '待验证';
-    case 'accept':
-      return '已添加';
-    case 'decline':
+    case 'accepted':
+      return '已接受';
+    case 'declined':
       return '已拒绝';
   }
 });
+
+const lcApp = new LC.App({
+  appId: 'R5EkvLasr16SMD4sJs3PROCf-gzGzoHsz',
+  appKey: 'KF3a4Xvk2HTcERPya1rSL9Fs',
+  serverURL: 'https://r5ekvlas.lc-cn-n1-shared.com',
+});
+
+const User = new LC.User(lcApp);
+const Friendship = new LC.Friendship(lcApp);
+
+function getErrorMessage(error) {
+  if (!(error instanceof Error)) {
+    return error.toString();
+  }
+  if (typeof error.error === 'string') {
+    return error.error;
+  } else {
+    return error.message;
+  }
+}
 
 function createApp(el) {
   return new Vue({
     el,
     template: '#friend-demo',
     data: {
+      tabIndex: 0,
       username: '',
       password: '',
-      tabIndex: 0,
       user: null,
-      tabIndex: 0,
-      targetObjectId: '',
-      myRequests: [],
+      targetUsername: '',
+      friends: [],
       requests: [],
       tip: '',
     },
-    mounted() {
-      const listener = (e, { from, to }) => {
-        if (to.objectId === this.$data.user.objectId) {
-          switch (e) {
-            case 'request':
-              this.$data.requests.push({ ...from, status: 'pending' });
-              break;
-            case 'accept':
-              {
-                const req = this.$data.myRequests.find((req) => req.objectId === from.objectId);
-                if (req) {
-                  req.status = 'accept';
-                }
-              }
-              break;
-            case 'decline':
-              {
-                const req = this.$data.myRequests.find((req) => req.objectId === from.objectId);
-                if (req) {
-                  req.status = 'decline';
-                }
-              }
-              break;
+    methods: {
+      upsertFriend(newFriend) {
+        for (let i = 0; i < this.$data.friends.length; ++i) {
+          const friend = this.$data.friends[i];
+          if (friend.objectId === newFriend.objectId) {
+            Object.assign(friend, newFriend);
+            return;
           }
         }
-      };
-      listeners.push(listener);
-    },
-    methods: {
-      handleLogin() {
+        this.$data.friends.push(newFriend);
+      },
+      upsertRequest(newRequest) {
+        for (let i = 0; i < this.$data.requests.length; ++i) {
+          const request = this.$data.requests[i];
+          if (request.objectId === newRequest.objectId) {
+            Object.assign(request, newRequest);
+            return;
+          }
+        }
+        this.$data.requests.unshift(newRequest);
+      },
+      removeFriend(objectId) {
+        for (let i = 0; i < this.$data.friends.length; ++i) {
+          const friend = this.$data.friends[i];
+          if (friend.objectId === newFriend.objectId) {
+            this.$data.friends.splice(i, 1);
+            return;
+          }
+        }
+      },
+      async fetchFriends() {
+        const friends = await Friendship.getFriends(this.user);
+        friends.forEach((friend) =>
+          this.upsertFriend({
+            objectId: friend.objectId,
+            username: friend.username,
+          })
+        );
+      },
+      async fetchRequests() {
+        const requests = await Friendship.getReceivedRequests(this.user, 'all', {
+          include: 'user',
+        });
+        requests.forEach((request) =>
+          this.upsertRequest({
+            objectId: request.objectId,
+            username: request.user.username,
+            status: request.status,
+          })
+        );
+      },
+      async subscribeFriendChange() {
+        const query = Friendship.getFriendQuery(this.user);
+        const liveQuery = await query.subscribe();
+        const handleRequestAccepted = async (request) => {
+          const friend = await User.object(request.data.followee.objectId).get();
+          this.upsertFriend({
+            objectId: request.data.followee.objectId,
+            username: friend.username,
+          });
+        };
+        liveQuery.on('create', () => console.log('create'));
+        liveQuery.on('enter', () => console.log('enter'));
+        liveQuery.on('update', () => console.log('update'));
+      },
+      async subscribeRequestChange() {
+        const query = Friendship.getReceivedRequestQuery(this.user);
+        const liveQuery = await query.subscribe();
+        liveQuery.on('create', async (request) => {
+          const user = await User.object(request.user.objectId).get();
+          this.upsertRequest({
+            objectId: request.objectId,
+            username: user.username,
+            status: request.status,
+          });
+        });
+      },
+      async handleLogin() {
         const { username, password } = this.$data;
         if (!username || !password) {
           this.$data.tip = '请输入用户名和密码';
           return;
         }
-        this.$data.user = {
-          username,
-          objectId: Math.random().toString(36).slice(-8),
-        };
+        try {
+          this.$data.user = await User.login(username, password);
+          this.fetchFriends();
+          this.fetchRequests();
+          this.subscribeFriendChange();
+          this.subscribeRequestChange();
+        } catch (error) {
+          this.$data.tip = getErrorMessage(error);
+        }
       },
-      handleSignUp() {
+      async handleSignUp() {
         const { username, password } = this.$data;
         if (!username || !password) {
           this.$data.tip = '请输入用户名和密码';
           return;
         }
-        this.$data.user = { username, password };
+        try {
+          this.$data.user = await User.signUp({ username, password });
+        } catch (error) {
+          this.$data.tip = getErrorMessage(error);
+        }
       },
-      handleSendRequest() {
-        const { targetObjectId } = this.$data;
-        if (!targetObjectId) {
-          this.$data.tip = '请输入对方的 objectId';
+      async handleSendRequest() {
+        const { targetUsername } = this.$data;
+        if (!targetUsername) {
+          this.$data.tip = '请输入对方的用户名';
           return;
         }
-        if (targetObjectId === this.$data.user.objectId) {
-          this.$data.tip = '不能添加自己为好友';
-          return;
+        try {
+          const target = await User.where('username', '==', targetUsername).first();
+          if (!target) {
+            this.$data.tip = '查无此人';
+            return;
+          }
+          await Friendship.request(this.user, target.objectId);
+          this.$data.tip = '请求已发送，等待对方确认';
+          this.$data.targetUsername = '';
+        } catch (error) {
+          this.$data.tip = getErrorMessage(error);
         }
-        this.$data.targetObjectId = '';
-        const req = {
-          from: this.$data.user,
-          to: { objectId: targetObjectId },
-        };
-        broadcast('request', req);
-        this.$data.myRequests.push({ ...req.to, status: 'pending' });
       },
-      handleAccept(user) {
-        broadcast('accept', {
-          from: this.$data.user,
-          to: user,
-        });
-        user.status = 'accept';
+      handleAccept(request) {
+        request.status = 'accepted';
+        Friendship.acceptRequest(this.user, request.objectId);
       },
-      handleDecline(user) {
-        broadcast('decline', {
-          from: this.$data.user,
-          to: user,
-        });
-        user.status = 'decline';
+      handleDecline(request) {
+        request.status = 'declined';
+        Friendship.declineRequest(this.user, request.objectId);
       },
     },
   });
