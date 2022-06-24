850c09df3cc18c4971c733d378fd7773d0b2af74
Lee Yeh <lee55962698@gmail.com>
2022-06-24 15:05:49 +0800

    fix(d.ts): remove the intermediate ErrorCode class

--- storage.d.ts
+++ storage.d.ts
@@ -1120,69 +1120,287 @@ export enum LeaderboardVersionChangeInterval {
   MONTH,
 }
 
-export class Error extends ErrorCode {
+export class Error {
   code: number;
   message: string;
+  rawMessage?: string;
 
   constructor(code: number, message: string);
-}
 
-declare class ErrorCode {
-  static OTHER_CAUSE: number;
-  static INTERNAL_SERVER_ERROR: number;
-  static CONNECTION_FAILED: number;
-  static OBJECT_NOT_FOUND: number;
-  static INVALID_QUERY: number;
-  static INVALID_CLASS_NAME: number;
-  static MISSING_OBJECT_ID: number;
-  static INVALID_KEY_NAME: number;
-  static INVALID_POINTER: number;
-  static INVALID_JSON: number;
-  static COMMAND_UNAVAILABLE: number;
-  static NOT_INITIALIZED: number;
-  static INCORRECT_TYPE: number;
-  static INVALID_CHANNEL_NAME: number;
-  static PUSH_MISCONFIGURED: number;
-  static OBJECT_TOO_LARGE: number;
-  static OPERATION_FORBIDDEN: number;
-  static CACHE_MISS: number;
-  static INVALID_NESTED_KEY: number;
-  static INVALID_FILE_NAME: number;
-  static INVALID_ACL: number;
-  static TIMEOUT: number;
-  static INVALID_EMAIL_ADDRESS: number;
-  static MISSING_CONTENT_TYPE: number;
-  static MISSING_CONTENT_LENGTH: number;
-  static INVALID_CONTENT_LENGTH: number;
-  static FILE_TOO_LARGE: number;
-  static FILE_SAVE_ERROR: number;
-  static DUPLICATE_VALUE: number;
-  static INVALID_ROLE_NAME: number;
-  static EXCEEDED_QUOTA: number;
-  static SCRIPT_FAILED: number;
-  static VALIDATION_ERROR: number;
-  static INVALID_IMAGE_DATA: number;
-  static UNSAVED_FILE_ERROR: number;
-  static INVALID_PUSH_TIME_ERROR: number;
-  static FILE_DELETE_ERROR: number;
-  static REQUEST_LIMIT_EXCEEDED: number;
-  static INVALID_EVENT_NAME: number;
-  static USERNAME_MISSING: number;
-  static PASSWORD_MISSING: number;
-  static USERNAME_TAKEN: number;
-  static EMAIL_TAKEN: number;
-  static EMAIL_MISSING: number;
-  static EMAIL_NOT_FOUND: number;
-  static SESSION_MISSING: number;
-  static MUST_CREATE_USER_THROUGH_SIGNUP: number;
-  static ACCOUNT_ALREADY_LINKED: number;
-  static INVALID_SESSION_TOKEN: number;
-  static LINKED_ID_MISSING: number;
-  static INVALID_LINKED_SESSION: number;
-  static UNSUPPORTED_SERVICE: number;
-  static AGGREGATE_ERROR: number;
-  static FILE_READ_ERROR: number;
-  static X_DOMAIN_REQUEST: number;
+  /**
+   * Error code indicating some error other than those enumerated here.
+   */
+  static OTHER_CAUSE: -1;
+
+  /**
+   * Error code indicating that something has gone wrong with the server.
+   * If you get this error code, it is AV's fault.
+   */
+  static INTERNAL_SERVER_ERROR: 1;
+
+  /**
+   * Error code indicating the connection to the AV servers failed.
+   */
+  static CONNECTION_FAILED: 100;
+
+  /**
+   * Error code indicating the specified object doesn't exist.
+   */
+  static OBJECT_NOT_FOUND: 101;
+
+  /**
+   * Error code indicating you tried to query with a datatype that doesn't
+   * support it, like exact matching an array or object.
+   */
+  static INVALID_QUERY: 102;
+
+  /**
+   * Error code indicating a missing or invalid classname. Classnames are
+   * case-sensitive. They must start with a letter, and a-zA-Z0-9_ are the
+   * only valid characters.
+   */
+  static INVALID_CLASS_NAME: 103;
+
+  /**
+   * Error code indicating an unspecified object id.
+   */
+  static MISSING_OBJECT_ID: 104;
+
+  /**
+   * Error code indicating an invalid key name. Keys are case-sensitive. They
+   * must start with a letter, and a-zA-Z0-9_ are the only valid characters.
+   */
+  static INVALID_KEY_NAME: 105;
+
+  /**
+   * Error code indicating a malformed pointer. You should not see this unless
+   * you have been mucking about changing internal AV code.
+   */
+  static INVALID_POINTER: 106;
+
+  /**
+   * Error code indicating that badly formed JSON was received upstream. This
+   * either indicates you have done something unusual with modifying how
+   * things encode to JSON, or the network is failing badly.
+   */
+  static INVALID_JSON: 107;
+
+  /**
+   * Error code indicating that the feature you tried to access is only
+   * available internally for testing purposes.
+   */
+  static COMMAND_UNAVAILABLE: 108;
+
+  /**
+   * You must call AV.initialize before using the AV library.
+   */
+  static NOT_INITIALIZED: 109;
+
+  /**
+   * Error code indicating that a field was set to an inconsistent type.
+   */
+  static INCORRECT_TYPE: 111;
+
+  /**
+   * Error code indicating an invalid channel name. A channel name is either
+   * an empty string (the broadcast channel) or contains only a-zA-Z0-9_
+   * characters.
+   */
+  static INVALID_CHANNEL_NAME: 112;
+
+  /**
+   * Error code indicating that push is misconfigured.
+   */
+  static PUSH_MISCONFIGURED: 115;
+
+  /**
+   * Error code indicating that the object is too large.
+   */
+  static OBJECT_TOO_LARGE: 116;
+
+  /**
+   * Error code indicating that the operation isn't allowed for clients.
+   */
+  static OPERATION_FORBIDDEN: 119;
+
+  /**
+   * Error code indicating the result was not found in the cache.
+   */
+  static CACHE_MISS: 120;
+
+  /**
+   * Error code indicating that an invalid key was used in a nested
+   * JSONObject.
+   */
+  static INVALID_NESTED_KEY: 121;
+
+  /**
+   * Error code indicating that an invalid filename was used for AVFile.
+   * A valid file name contains only a-zA-Z0-9_. characters and is between 1
+   * and 128 characters.
+   */
+  static INVALID_FILE_NAME: 122;
+
+  /**
+   * Error code indicating an invalid ACL was provided.
+   */
+  static INVALID_ACL: 123;
+
+  /**
+   * Error code indicating that the request timed out on the server. Typically
+   * this indicates that the request is too expensive to run.
+   */
+  static TIMEOUT: 124;
+
+  /**
+   * Error code indicating that the email address was invalid.
+   */
+  static INVALID_EMAIL_ADDRESS: 125;
+
+  /**
+   * Error code indicating a missing content type.
+   */
+  static MISSING_CONTENT_TYPE: 126;
+
+  /**
+   * Error code indicating a missing content length.
+   */
+  static MISSING_CONTENT_LENGTH: 127;
+
+  /**
+   * Error code indicating an invalid content length.
+   */
+  static INVALID_CONTENT_LENGTH: 128;
+
+  /**
+   * Error code indicating a file that was too large.
+   */
+  static FILE_TOO_LARGE: 129;
+
+  /**
+   * Error code indicating an error saving a file.
+   */
+  static FILE_SAVE_ERROR: 130;
+
+  /**
+   * Error code indicating an error deleting a file.
+   */
+  static FILE_DELETE_ERROR: 153;
+
+  /**
+   * Error code indicating that a unique field was given a value that is
+   * already taken.
+   */
+  static DUPLICATE_VALUE: 137;
+
+  /**
+   * Error code indicating that a role's name is invalid.
+   */
+  static INVALID_ROLE_NAME: 139;
+
+  /**
+   * Error code indicating that an application quota was exceeded.  Upgrade to
+   * resolve.
+   */
+  static EXCEEDED_QUOTA: 140;
+
+  /**
+   * Error code indicating that a Cloud Code script failed.
+   */
+  static SCRIPT_FAILED: 141;
+
+  /**
+   * Error code indicating that a Cloud Code validation failed.
+   */
+  static VALIDATION_ERROR: 142;
+
+  /**
+   * Error code indicating that invalid image data was provided.
+   */
+  static INVALID_IMAGE_DATA: 150;
+
+  /**
+   * Error code indicating an unsaved file.
+   */
+  static UNSAVED_FILE_ERROR: 151;
+
+  /**
+   * Error code indicating an invalid push time.
+   */
+  static INVALID_PUSH_TIME_ERROR: 152;
+
+  /**
+   * Error code indicating that the username is missing or empty.
+   */
+  static USERNAME_MISSING: 200;
+
+  /**
+   * Error code indicating that the password is missing or empty.
+   */
+  static PASSWORD_MISSING: 201;
+
+  /**
+   * Error code indicating that the username has already been taken.
+   */
+  static USERNAME_TAKEN: 202;
+
+  /**
+   * Error code indicating that the email has already been taken.
+   */
+  static EMAIL_TAKEN: 203;
+
+  /**
+   * Error code indicating that the email is missing, but must be specified.
+   */
+  static EMAIL_MISSING: 204;
+
+  /**
+   * Error code indicating that a user with the specified email was not found.
+   */
+  static EMAIL_NOT_FOUND: 205;
+
+  /**
+   * Error code indicating that a user object without a valid session could
+   * not be altered.
+   */
+  static SESSION_MISSING: 206;
+
+  /**
+   * Error code indicating that a user can only be created through signup.
+   */
+  static MUST_CREATE_USER_THROUGH_SIGNUP: 207;
+
+  /**
+   * Error code indicating that an an account being linked is already linked
+   * to another user.
+   */
+  static ACCOUNT_ALREADY_LINKED: 208;
+
+  /**
+   * Error code indicating that a user cannot be linked to an account because
+   * that account's id could not be found.
+   */
+  static LINKED_ID_MISSING: 250;
+
+  /**
+   * Error code indicating that a user with a linked (e.g. Facebook) account
+   * has an invalid session.
+   */
+  static INVALID_LINKED_SESSION: 251;
+
+  /**
+   * Error code indicating that a service being linked (e.g. Facebook or
+   * Twitter) is unsupported.
+   */
+  static UNSUPPORTED_SERVICE: 252;
+
+  /**
+   * Error code indicating a real error code is unavailable because
+   * we had to use an XDomainRequest object to allow CORS requests in
+   * Internet Explorer, which strips the body from HTTP responses that have
+   * a non-2XX status code.
+   */
+  static X_DOMAIN_REQUEST: 602;
 }
 
 /**
